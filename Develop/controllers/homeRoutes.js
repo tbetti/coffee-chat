19480e9704d25cc4afc627b9dd075acca332e472
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-09-18 11:24:42 -0400

    Connected login.handlebars to a route

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 
+router.get('/', (req, res)=>{
+    try{
+        res.render('login');
+    }catch(err){
+        res.json(err)
+    }
+});
+
 module.exports = router;
